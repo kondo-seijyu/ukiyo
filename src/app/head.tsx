e36647e247825996940e99df3bb798a2ab0670b3
Seijyu Kondo <kondo@reflection-inc.com>
2025-07-10 01:55:19 +0900

    work3調整

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -9,14 +9,14 @@ export default function Head() {
       <meta property="og:title" content="UKIYO – 技術と余白で描くポートフォリオ" />
       <meta property="og:description" content="静と動が調和する“余白美”あるインターフェースを体験できるポートフォリオ。" />
       <meta property="og:url" content="https://ukiyo-reflection.vercel.app" />
-      <meta property="og:image" content="https://ukiyo-reflection.vercel.app/hero/image_ukiyo.png" />
+      <meta property="og:image" content="https://ukiyo-reflection.vercel.app/work/work3.png" />
 
       {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@your_twitter_handle" />
       <meta name="twitter:title" content="UKIYO – 技術と余白で描くポートフォリオ" />
       <meta property="og:image" content="https://ukiyo-reflection.vercel.app/hero/image_ukiyo.png" />
-      <meta name="twitter:image" content="https://ukiyo-reflection.vercel.app/hero/image_ukiyo.png" />
+      <meta name="twitter:image" content="https://ukiyo-reflection.vercel.app/work/work3.png" />
 
       {/* その他 */}
       <meta name="theme-color" content="#ffffff" />
